f3224ff920fbb491a64bcf9253ef6816479ee7f5
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-08-11 19:49:43 +0300

    S3A

--- v3/Services/Service_Three_A.java
+++ v3/Services/Service_Three_A.java
@@ -1,7 +1,19 @@
 package com.company.v3.Services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Service;
+
+@Service(value = "Service_Three_A")
+@Scope(value = "prototype",proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class Service_Three_A {
     private String one;
+    @Autowired
+    public Service_Three_A(@Value("${prop.s3a}") String one) {
+        this.one = one;
+    }
 
     public String getOne() {
         return one;
